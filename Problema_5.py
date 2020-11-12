661b616aeec4feb4d1a8377ed0cd24223392acad
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-11-12 14:33:56 +0200

    Add files via upload

---
+++ Problema_5.py
@@ -0,0 +1,25 @@
+n = int(input("Anul:"))
+if n%12 == 0:
+  print("Maimuță")
+if n%12 == 1:
+  print("Cocoș")
+if n%12 == 2:
+  print("Câine")
+if n%12 == 3:
+  print("Porc")
+if n%12 == 4:
+  print("Șobolan")
+if n%12 == 5:
+  print("Bou")
+if n%12 == 6:
+  print("Tigru")
+if n%12 == 7:
+  print("Iepure")
+if n%12 == 8:
+  print("Dragon")
+if n%12 == 9:
+  print("Șarpe")
+if n%12 == 10:
+  print("Cal")
+if n%12 == 11:
+  print("Oaie")

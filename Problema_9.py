685ad8bf3ff0be11ab1f2b5bb2037c30f8781743
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-11-18 16:35:29 +0200

    Update Problema_9.py

--- Problema_9.py
+++ Problema_9.py
@@ -1,7 +1,7 @@
 n=int(input("n:"))
 suma=0
-for x in range(3,n+1):
-    for y in range(1,n+1):
+for x in range(3,n):
+    for y in range(1,n):
         if y<=x:
             if x%y==0 and x!=y:
                 suma+=y

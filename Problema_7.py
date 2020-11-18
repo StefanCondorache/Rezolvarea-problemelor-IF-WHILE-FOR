fa9eb4543df6e3a6831fd3461fd22d5d72779be8
StefanCondorache <70690185+StefanCondorache@users.noreply.github.com>
2020-11-18 16:33:55 +0200

    Update Problema_7.py

--- Problema_7.py
+++ Problema_7.py
@@ -1,17 +1,12 @@
 n=int(input("Vârsta:"))
 if n<20:
-    suma=1
     dolari=1
     for x in range(1,n+1):
-        dolari*=2
-        suma+=dolari+x
-    print("suma=",suma)
-    suma=1
+        dolari=(dolari*2)+x
+    print(dolari,'$')
     dolari=1
     for x in range(1,n+1):
-        dolari*=2
-        suma+=dolari+x   
-        if suma>100:
-            print("La vârsta de",x,"ani cadoul a fost mai mare de 100$")
+        dolari=(dolari*2)+x
+        if dolari>100:
+            print("La vârsta de",x,"ani cadoul a devenit mai mare de 100$")
             break
-    
